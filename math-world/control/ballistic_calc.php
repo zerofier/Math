08a9a22de55d8c77de6c092b8f9fce408a477d96
Cho Kairin <zhao-hailin@fujielectric.com>
2020-07-10 17:34:20 +0900

    add r to ballistic_calc.php

--- math-world/control/ballistic_calc.php
+++ math-world/control/ballistic_calc.php
@@ -34,52 +34,84 @@ $dispatcher->route('/math-world/'.basename(__FILE__, '.php'), function() {
 	init();
 
 	</script>
-	<div>`(dy)/(dx) = g - p y^2`</div>
-	<div>`int ((dy)/(dx)) / (g - p y^2) dx = int 1 dx`</div>
-	<div>`int (dy) / (g - p y^2) = x`</div>
-	<div>`int 1 / (g - p y^2) dy = x + C_1`</div>
-	<div>`int 1 / (g (1 - (p y^2) / g)) dy = x + C_1`</div>
-	<div>`1 / g int 1 / (1 - (p y^2) / g) dy = x + C_1`</div>
-	
-	<div>`u = (i sqrt p  y) / (sqrt g)`</div>
-	<div>`du = (i sqrt p) / (sqrt g) dy`</div>
-	<div>`dy = du (sqrt g) / (i sqrt p)`</div>
-	<div>`- (p y^2) / g = u^2`</div>
-	<div>代入</div>
-	
-	<div>`1 / g int 1 / (1 + u^2) du (sqrt g) / (i sqrt p) = x + C_1`</div>
-	<div>`(1 / g) (sqrt g) / (i sqrt p) int 1 / (1 + u^2) du = x + C_1`</div>
-	<div>`-i / (sqrt (gp)) int 1 / (1 + u^2) du = x + C_1`</div>
-	
-	<div>`-i / (sqrt (gp)) (tan^(-1)(u) + C_2) = x + C_1`</div>
-	<div>逆代入</div>
-	<div>`-i / (sqrt (gp)) (tan^(-1)((i sqrt p  y) / (sqrt g)) + C_2) = x + C_1`</div>
-	<div>`itanh(1) = tan(i)`によって</div>
-	<div>`-i / (sqrt (gp)) (tan^(-1)((i sqrt p  y) / (sqrt g)) + C_2) = x + C_1`</div>
-	<div>`-i / (sqrt (gp)) (i tanh^(-1)(sqrt (p  / g) y) + C_2) = x + C_1`</div>
-	<div>`1 / (sqrt (gp)) tanh^(-1)(sqrt (p  / g) y) -i / (sqrt (gp)) C_2 = x + C_1`</div>
-	<div>`tanh^(-1)(sqrt (p  / g) y) -i C_2 = (x + C_1) / (sqrt (gp))`</div>
-	<div>`tanh^(-1)(sqrt (p  / g) y) = (x + C_1) / (sqrt (gp)) + i C_2`</div>
-	<div>`1/2 ln ((1 + sqrt (p  / g) y)/(1- sqrt (p  / g) y))= (x + C_1) / (sqrt (gp)) + i C_2`</div>
-	<div>`(1 + sqrt (p  / g) y)/(1- sqrt (p  / g) y) = exp (2 ((x + C_1) / (sqrt (gp)) + i C_2))`</div>
-	<div>`(1 + sqrt (p  / g) y)= (exp (2 ((x + C_1) / (sqrt (gp)) + i C_2))) (1- sqrt (p  / g) y)`</div>
-	<div>`(1 + sqrt (p  / g) y)= exp (2 ((x + C_1) / (sqrt (gp)) + i C_2))- sqrt (p  / g) y exp (2 ((x + C_1) / (sqrt (gp)) + i C_2))`</div>
-	
-	<div>`1 + sqrt (p  / g) y + sqrt (p  / g) y exp (2 ((x + C_1) / (sqrt (gp)) + i C_2))= exp (2 ((x + C_1) / (sqrt (gp)) + i C_2))`</div>
-	
-	<div>`1 + sqrt (p  / g) y (1+exp (2 ((x + C_1) / (sqrt (gp)) + i C_2)))= exp (2 ((x + C_1) / (sqrt (gp)) + i C_2))`</div>
-	
-	<div>`sqrt (p  / g) y = (exp (2 ((x + C_1) / (sqrt (gp)) + i C_2)) - 1) / (exp (2 ((x + C_1) / (sqrt (gp)) + i C_2 )) + 1)`</div>
-	
-	<div>`y = sqrt (g / p) (exp (2 ((x + C_1) / (sqrt (gp)) + i C_2)) - 1) / (exp (2 ((x + C_1) / (sqrt (gp)) + i C_2 )) + 1)`</div>
-	
-	<div>`y = sqrt (g / p) (exp (2 ((x + C_1) / (sqrt (gp)) + i C_2)) - 1) / (exp (2 ((x + C_1) / (sqrt (gp)) + i C_2 )) + 1)`</div>
-	
-	<div>`y = sqrt (g / p) tanh ((x + C_1) / (sqrt (gp)) + i C_2)`</div>
-	
-	<div>`y = sqrt (g / p) tanh (x / (sqrt (gp)) +  C_1 / (sqrt (gp)) + i C_2)`</div>
-	
-	<div>`y = sqrt (g / p) tanh (x / (sqrt (gp)) + C)`</div>
+	<style>
+		#basic-math > div{
+			margin-bottom: 1em
+		}
+	</style>
+	<div id="basic-math">
+		<div>`(dy)/(dx) = g - p y^2`</div>
+		<div>`int ((dy)/(dx)) / (g - p y^2) dx = int 1 dx`</div>
+		<div>`int (dy) / (g - p y^2) = x + C_1`</div>
+		<div>`int 1 / (g - p y^2) dy = x + C_1`</div>
+		<div>`int 1 / (g (1 - (p y^2) / g)) dy = x + C_1`</div>
+		<div>`1 / g int 1 / (1 - (p y^2) / g) dy = x + C_1`</div>
+		
+		<div>`u = (i sqrt p  y) / (sqrt g)`</div>
+		<div>`du = (i sqrt p) / (sqrt g) dy`</div>
+		<div>`dy = du (sqrt g) / (i sqrt p)`</div>
+		<div>`- (p y^2) / g = u^2`</div>
+		<div>代入</div>
+		
+		<div>`1 / g int 1 / (1 + u^2) du (sqrt g) / (i sqrt p) = x + C_1`</div>
+		<div>`(1 / g) (sqrt g) / (i sqrt p) int 1 / (1 + u^2) du = x + C_1`</div>
+		<div>`-i / (sqrt (gp)) int 1 / (1 + u^2) du = x + C_1`</div>
+		
+		<div>`-i / (sqrt (gp)) (tan^(-1)(u) + C_2) = x + C_1`</div>
+		
+		<div>`-i / (sqrt(gp)) tan^(-1)(u) = x + C_1 + C_2 i / (sqrt(gp))`</div>
+		<div>`-i / (sqrt(gp)) tan^(-1)(u) = x + C`</div>
+		<div>逆代入</div>
+		<div>`-i / (sqrt(gp)) tan^(-1)((i sqrt p y) / (sqrt g)) = x + C`</div>
+		<div>`itanh(1) = tan(i)`によって</div>
+		<div>`-i / (sqrt(gp)) tan^(-1)((i sqrt p y) / (sqrt g)) = x + C`</div>
+		<div>`-i / (sqrt(gp)) i tanh^(-1)(sqrt(p / g) y) = x + C`</div>
+		<div>`1 / (sqrt(gp)) tanh^(-1)(sqrt(p / g) y)  = x + C`</div>
+		<div>`tanh^(-1)(sqrt (p  / g) y) = (x + C) sqrt(gp)`</div>
+		
+		<div>`1/2 ln((1 + sqrt(p / g) y)/(1- sqrt(p / g) y))= sqrt(gp) (x + C) `</div>
+		
+		<div>`(1 + sqrt(p / g) y)/(1- sqrt (p / g) y) = exp (2 sqrt(gp) (x + C))`</div>
+		<div>`(1 + sqrt(p / g) y) = exp(2 sqrt(gp) (x + C)) (1- sqrt(p / g) y)`</div>
+		<div>`(1 + sqrt(p / g) y) = exp(2 sqrt(gp) (x + C)) - sqrt(p / g) y exp(2 sqrt(gp) (x + C))`</div>
+		
+		<div>`1 + sqrt(p / g) y + sqrt(p / g) y exp(2 sqrt(gp) (x + C)) = exp(2 sqrt(gp) (x + C))`</div>
+		
+		<div>`1 + sqrt(p / g) y (1 + exp(2 sqrt(gp) (x + C)))= exp(2 sqrt(gp) (x + C))`</div>
+		
+		<div>`sqrt(p / g) y = (exp(2 sqrt(gp) (x + C)) - 1) / (exp(2 sqrt(gp) (x + C)) + 1)`</div>
+		
+		<div>`sqrt (p / g) y = tanh (sqrt(gp) (x + C))`</div>
+		
+		<div>`y = sqrt (g / p) tanh (sqrt(gp) (x + C))`</div>
+		
+		<div>初期状態`x=0, y=0`</div>
+		<div>`0 = C`</div>
+		<div>`y = sqrt (g / p) tanh (sqrt(gp) x)`&nbsp;(`g`は重力加速度, `p=P/M`の`M`は質量, `P=1/2 rho S C_D`)</div>
+		
+		<div>`int sqrt (g / p) tanh (sqrt(gp) x) dx`</div>
+		
+		<div>`sqrt (g / p) int tanh (sqrt(gp) x) dx`</div>
+		<div>`u = sqrt(gp) x`</div>
+		<div>`du = sqrt(gp) dx`</div>
+		<div>代入</div>
+		<div>`sqrt (g / p) 1/sqrt(gp) int tanh (u) du`</div>
+		<div>`1/p int tanh (u) du`</div>
+		<div>`1/p ln(exp(u) + exp(-u)) ＋ C`</div>
+		<div>逆代入</div>
+		<div>`r = 1/p ln(exp(sqrt(gp) x) + exp(-sqrt(gp) x)) + C`</div>
+		
+		<div>初期状態`x=0, r=0`</div>
+		<div>`0 = 1/p ln(exp(0) + exp(0)) + C`</div>
+		<div>`0 = 1/p ln(1 + 1) + C`</div>
+		<div>`C = -1/p ln(2)`</div>
+		
+		<div>`r = 1/p ln(exp(sqrt(gp) x) + exp(-sqrt(gp) x)) - 1/p ln(2)`</div>
+		
+		<div>`r = 1/p ln(1/2(exp(sqrt(gp) x) + exp(-sqrt(gp) x)))`</div>
+		
+		<div>`r = 1/p ln(cosh(sqrt(gp) x))`</div>
+	</div>
 	<!--
 	<div>`tanh(x) = (e^x - e^(-x)) / (e^x + e^(-x))`</div>
 	-->
